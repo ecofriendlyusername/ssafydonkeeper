e3736485f2bec5fff35d1f8d7ce7b56a18d65519
정민지 <wjdalswl0731@gmail.com>
2023-04-21 10:02:24 +0900

    S08P31C209 fix: backend

---
+++ moneykeeperbackend/src/main/java/com/ssafy/moneykeeperbackend/MoneykeeperbackendApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.moneykeeperbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneykeeperbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneykeeperbackendApplication.class, args);
+	}
+
+}
---
+++ moneykeeperbackend/src/test/java/com/ssafy/moneykeeperbackend/MoneykeeperbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.moneykeeperbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneykeeperbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
