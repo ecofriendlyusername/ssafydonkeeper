9716e27039c1bf908ca848ed8c2d59ecf7a8e8ce
정민지 <wjdalswl0731@gmail.com>
2023-04-27 15:10:32 +0900

    S08P31C209-146 hotfix: SecurityConfig.java

--- moneykeeperbackend/src/main/java/com/ssafy/moneykeeperbackend/security/config/SecurityConfig.java
+++ moneykeeperbackend/src/main/java/com/ssafy/moneykeeperbackend/security/config/SecurityConfig.java
@@ -83,7 +83,8 @@ public class SecurityConfig {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/**")
-					.allowedOrigins("http://127.0.0.1:8081", "http://localhost:8081", "http://localhost:8080", "http://127.0.0.1:8080")
+					.allowedOrigins("http://127.0.0.1:8081", "http://localhost:8081", "http://localhost:8080", "http://127.0.0.1:8080",
+						"http://127.0.0.1:3000", "http://localhost:3000")
 					.allowedMethods("*")
 					.allowedHeaders("*")
 					.allowCredentials(true);
