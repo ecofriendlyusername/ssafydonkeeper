bcf192ad04048ab7e0f152a0d86f53f1492c9400
prefer_choi <prefer_choi@naver.com>
2023-05-01 17:55:43 +0900

    feat:researchPageDataSetup

--- frontend/src/views/research/researchView.vue
+++ frontend/src/views/research/researchView.vue
@@ -2,21 +2,62 @@
   <div>
       <h1>나의 최근 소비 패턴</h1>
     <div>
-      차트이미지
+      {{ research_data }}
     </div>
+  </div>
 
-    <RecomView/>
+  <div>
+    <h2>소비 맞춤 카드 추천</h2>
+    <div v-for="recom_card in recom_cards" :key="recom_card.id">
+      <img src="" alt=recom_card.path >
+      <div>{{ recom_card.name }} </div>
+      <div>{{ recom_card.company }} </div>
+    </div>
     
-
   </div>
 </template>
 
 <script>
-import RecomView from './recomView.vue'
+
 
 export default {
-  components:{
-    RecomView
+  data(){
+    return {
+      research_data: [
+        {
+          classification: '여행',
+          percent: 56
+        },
+        {
+          classification: '외식',
+          percent: 30
+        },
+        {
+          classification: '쇼핑',
+          percent: 15
+        },
+      ],
+      recom_cards: [
+        {
+          id: '1',
+          img_path: 'path',
+          name: '삼성카드 어쩌고 저쩌고 카드',
+          company: '삼성카드'
+        },
+        {
+          id: '2',
+          img_path: 'path',
+          name: '우리카드 그시기 므시기 카드',
+          company: '우리카드'
+        },
+        {
+          id: '3',
+          img_path: 'path',
+          name: '국민카드 가나다라마바사아자차카드',
+          company: '국민카드'
+        },
+      ]
+    }
   }
 
 }
