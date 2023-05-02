ec36a892689a0d97e1fb6b3052eb7fdf86a3cff7
양윤정 <yyj5126@naver.com>
2023-05-03 05:01:30 +0900

    Update main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,8 +3,5 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-import axios from 'axios';
-
-axios.defaults.withCredentials = true;
 
 createApp(App).use(store).use(router).mount('#app')
