9b6a9bf4526bb4951eb7d1380ec246fd8a8247a6
양윤정 <yyj5126@naver.com>
2023-05-07 21:13:06 +0900

    Update vue.config.js

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
-    port: 3030,
+    port: 3000,
     allowedHosts: "all",
   }
 })
