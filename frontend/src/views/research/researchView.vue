e4ddc9112e427c990aba192766afee0beee1cd18
prefer_choi <prefer_choi@naver.com>
2023-05-11 17:15:24 +0900

    feat:fillteringCardNum<3,ifNum>3ChangeETC.

--- frontend/src/views/research/researchView.vue
+++ frontend/src/views/research/researchView.vue
@@ -10,14 +10,14 @@
       </div>
       <div style="font-size:17px;">
         <h3>가장 많이 사용한 곳</h3>
-        <p v-for="(data, idx) in research_data" :key="idx">ㆍ{{ data.classification }} {{ data.percent }}%  </p>
+        <p v-for="(data, idx) in spend_data.filter((_, idx) => idx < key)" :key="idx">ㆍ{{ data.category }} {{ data.amount }} 원  </p>
       </div>
     </div>
 
     <div>
       <h2>카테고리별 사용 내역</h2>
 
-      <div v-for="(data, idx) in spend_data" :key = idx>
+      <div v-for="(data, idx) in spend_data.filter((el) => el.amount > 0)" :key = idx>
         <div>
           {{ data.category }} : {{ data.amount }}
         </div>
@@ -28,13 +28,13 @@
     <div style="margin-top:55px;">
       <h1>소비 맞춤 카드 추천</h1>
       
-      <div v-for="recom_card in recom_cards" :key="recom_card.id" class="cell" v-on:click="this.$router.push('/research/card/' + recom_card.id)" style="background-color:#E7E7E7; border-color:#E7E7E7 ;">
+      <div v-for="recom_card in recom_cards.filter((_, idx) => idx < 3)" :key="recom_card.id" class="cell" v-on:click="this.$router.push('/research/card/' + recom_card.id)" style="background-color:#E7E7E7; border-color:#E7E7E7 ;">
         <div style="display: flex; justify-content: space-between; background-color:white; padding: 10px; margin: 5px;">
           <div>
-            <!-- <img :src="recom_card.imgPath" alt=recom_card.imgPath> -->
+            <img :src="recom_card.imgPath" :alt=recom_card.imgPath  @load="onImageLoad" class="cardImg">
           </div>
           <div>
-            <div style="display:flex; font-weight: bold;">
+            <div style="display:flex; font-weight: bold; overflow:hidden; text-overflow: ellipsis; white-space: nowrap; max-width: 100px;">
               {{ recom_card.name }}
             </div>
             <div style="display:flex">
@@ -54,28 +54,22 @@ import Chart from 'chart.js/auto';
 
 export default {
   data() {
-    return {
+    return {  
       year:2023,
       month:5,
-      research_data: [
-        {
-          classification: '여행',
-          percent: 56
-        },
-        {
-          classification: '외식',
-          percent: 30
-        },
-        {
-          classification: '쇼핑',
-          percent: 15
-        },
-      ],
       spend_data:[],
-      recom_cards: []
+      recom_cards: [],
+      key: 4
     }
   },
   methods:{
+    onImageLoad(event){
+      if(event.target.width < event.target.height){
+        event.target.setAttribute("class", "trun cardImg");
+      }
+      
+      
+    },
     getData() {
       this.axios.get(process.env.VUE_APP_API_URL + `/statistics/monthlyspendingbycat/${this.year}/${this.month}`)
       .then(res=>{
@@ -83,37 +77,53 @@ export default {
         this.spend_data = res.data
       })
       .then(() => {
-        this.pieChartAdd()
+        this.axios.get(process.env.VUE_APP_API_URL + `/card/cards`  )
+        .then(res => {
+          this.recom_cards = res.data
+          console.log(this.recom_cards)
+        })
       })
-
-      this.axios.get(process.env.VUE_APP_API_URL + `/card/cards`  )
-      .then(res => {
-        this.recom_cards = res.data
-        console.log(this.recom_cards)
+      .then(() => {
+        this.pieChartAdd()
       })
     },
     pieChartAdd() {
-      new Chart(
-        document.getElementById('chart'),
-        {
-          type: 'pie',
-          options: {
-            plugins: {
-              legend: {
-                display: false
+      return new Promise((resolve) => {
+        const temp = this.spend_data.filter((_, idx) => idx < this.key)
+        console.log(temp);
+        temp.push({
+          'category': 'etc', 
+          'amount': this.spend_data.filter((_, idx) => idx > this.key).reduce((accumulator, currentValue) => accumulator + currentValue.amount, 0)
+        })
+        resolve(temp);
+      })
+      .then((temp) => {
+        new Chart(
+          document.getElementById('chart'),
+          {
+            type: 'pie',
+            options: {
+              plugins: {
+                legend: {
+                  display: false
+                }
               }
+            },
+            data: {
+              labels: temp.map(row => row.category),
+              datasets: [
+                {
+                  data: temp.map(row => row.amount)
+
+                }
+              ]
             }
-          },
-          data: {
-            labels: this.research_data.map(row => row.classification),
-            datasets: [
-              {
-                data: this.research_data.map(row => row.percent)
-              }
-            ]
           }
-        }
-      );
+        );
+      })
+      .catch((error) => {
+        console.error("An error occurred:", error);
+      });
     },
   },
   mounted() {
@@ -127,5 +137,11 @@ export default {
 .cell {
   display: block;
   border: black solid 1px;
+}
+.cardImg {
+  width: 150px;
+}
+.trun {
+  transform: rotate(90deg);
 }
 </style>
