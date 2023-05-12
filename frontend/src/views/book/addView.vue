0270e29a7adb97f9c0d07c77b5040af000a6eb04
prefer_choi <prefer_choi@naver.com>
2023-05-12 09:45:48 +0900

    feat:getData,axiosUpdate

--- frontend/src/views/book/addView.vue
+++ frontend/src/views/book/addView.vue
@@ -1,17 +1,18 @@
 <template>
   <div class="DivH">
     {{ income }}
-    <div v-on:click="income = true">수입</div> <div v-on:click="income=false">지출</div>
+    <div v-on:click="income = true" v-if="!income">수입</div>
+    <div v-on:click="income = false" v-else>지출</div>
     <br>
     <label for="assetId">assetId: </label><input type="text" id="assetId"><br><br>
-    <label for="incomeClassificationId">incomeClassificationId: </label><input type="text" id="incomeClassificationId"><br><br>
+    <label for="classificationId">classificationId: </label><input type="text"
+      id="classificationId"><br><br>
     <label for="date">date: </label><input type="date" id="date"><br><br>
     <label for="amount">amount: </label><input type="number" id="amount"><br><br>
     <label for="detail">detail: </label><input type="text" id="detail"><br><br>
     <label for="memo">memo: </label><input type="text" id="memo"><br><br>
-    <button v-on:click=add()>저장하기</button>
+    <button v-on:click=postData()>저장하기</button>
   </div>
-  
 </template>
 
 <script>
@@ -20,23 +21,42 @@ export default {
   data() {
     return {
       income: true,
+
     }
   },
   methods: {
-    getData(){
-      this.axios.get(process.env.VUE_APP_API_URL + ``)
+    getData() {
+      // 소득 분류 가져오기
+      this.axios.get(process.env.VUE_APP_API_URL + `/account-book/incomeclassification`)
+        .then((res) => {
+          console.log(res.data)
+        })
+        .catch((err) => {console.log(err)})
+      // 소비 분류 가져오기
+      this.axios.get(process.env.VUE_APP_API_URL + `/account-book/spendingclassification`)
+        .then((res) => {
+          console.log(res.data)
+        })
+        .catch((err) => {console.log(err)})
+      // 자산 분류 가져오기
+        this.axios.get(process.env.VUE_APP_API_URL + `/account-book/asset`)
+        .then((res) => {
+          console.log(res.data)
+        })
+        .catch((err) => {console.log(err)})
+      
     },
 
-    add(){
+    postData() {
       if (this.income) {
         this.spendadd()
-      }else{
+      } else {
         this.incomeadd()
       }
     },
     incomeadd() {
       console.log(document.querySelector("#assetId").value)
-      console.log(document.querySelector("#incomeClassificationId").value)
+      console.log(document.querySelector("#classificationId").value)
       console.log(document.querySelector("#date").value)
       console.log(document.querySelector("#amount").value)
       console.log(document.querySelector("#detail").value)
@@ -46,24 +66,24 @@ export default {
         method: 'post',
         url: process.env.VUE_APP_API_URL + `/account-book/income`,
         data: {
-          "assetId" : document.querySelector("#assetId").value,
-          "incomeClassificationId" : document.querySelector("#incomeClassificationId").value,
-          "date" : document.querySelector("#date").value,
-          "amount" : document.querySelector("#amount").value, 
+          "assetId": document.querySelector("#assetId").value,
+          "classificationId": document.querySelector("#classificationId").value,
+          "date": document.querySelector("#date").value,
+          "amount": document.querySelector("#amount").value,
           "detail": document.querySelector("#detail").value,
           "memo": document.querySelector("#memo").value
         },
       })
-      .then((res) => {
-        console.log("add")
-        console.log(res.data)
-      })
-      .catch(err=>{console.log(err)})
+        .then((res) => {
+          console.log("add")
+          console.log(res.data)
+        })
+        .catch(err => { console.log(err) })
     },
 
     spendadd() {
       console.log(document.querySelector("#assetId").value)
-      console.log(document.querySelector("#incomeClassificationId").value)
+      console.log(document.querySelector("#classificationId").value)
       console.log(document.querySelector("#date").value)
       console.log(document.querySelector("#amount").value)
       console.log(document.querySelector("#detail").value)
@@ -73,23 +93,23 @@ export default {
         method: 'post',
         url: process.env.VUE_APP_API_URL + `/account-book/spending`,
         data: {
-          "assetId" : document.querySelector("#assetId").value,
-          "spendingClassificationId" : document.querySelector("#incomeClassificationId").value,
-          "date" : document.querySelector("#date").value,
-          "amount" : document.querySelector("#amount").value, 
+          "assetId": document.querySelector("#assetId").value,
+          "classificationId": document.querySelector("#classificationId").value,
+          "date": document.querySelector("#date").value,
+          "amount": document.querySelector("#amount").value,
           "detail": document.querySelector("#detail").value,
           "memo": document.querySelector("#memo").value
         },
       })
-      .then((res) => {
-        console.log("add")
-        console.log(res.data)
-      })
-      .catch(err=>{console.log(err)})
+        .then((res) => {
+          console.log("add")
+          console.log(res.data)
+        })
+        .catch(err => { console.log(err) })
     },
   },
   mounted() {
-    // this.getData()
+    this.getData()
   },
 
 }
