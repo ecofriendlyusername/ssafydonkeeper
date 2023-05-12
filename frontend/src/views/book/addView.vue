c47b049fbd40cc0a014039058e3445527439db70
prefer_choi <prefer_choi@naver.com>
2023-05-12 14:58:54 +0900

    feat:addModal

--- frontend/src/views/book/addView.vue
+++ frontend/src/views/book/addView.vue
@@ -1,12 +1,43 @@
 <template>
   <div class="DivH">
-    {{ income }}
-    <div v-on:click="income = true" v-if="!income">수입</div>
-    <div v-on:click="income = false" v-else>지출</div>
+    <h2 v-on:click="flag = !flag" v-if="flag">수입</h2>
+    <h2 v-on:click="flag = !flag" v-else>지출</h2>
+    <h4 v-on:click="flag = !flag">바꾸기</h4>
     <br>
-    <label for="assetId">assetId: </label><input type="text" id="assetId"><br><br>
-    <label for="classificationId">classificationId: </label><input type="text"
-      id="classificationId"><br><br>
+    <label for="assetId">assetId: </label><div type="text" id="assetId" :value="AC.name"
+      v-on:click="aModalFlag = !aModalFlag">{{ AC.name }}</div>
+
+    <div class="modal" v-show="aModalFlag">
+      <ul>
+        <h4>소득 분류</h4>
+        <li v-for="el in AC_data" :key="el.id" v-on:click="AC = el">
+          {{ el.name }}
+        </li>
+      </ul>
+    </div>
+
+    <br><br>
+    <label for="classificationId">classificationId: </label>
+    <div type="text" id="classificationId" :value="flag ? IC.name : SC.name" v-on:click="cModalFlag = !cModalFlag">{{ flag ? IC.name : SC.name }}</div>
+
+    <div class="modal" v-show="cModalFlag && !flag">
+      <ul>
+        <h4>지출 분류</h4>
+        <li v-for="el in SC_data" :key="el.id" v-on:click="SC = el">
+          {{ el.name }}
+        </li>
+      </ul>
+    </div>
+
+    <div class="modal" v-show="cModalFlag && flag">
+      <ul>
+        <h4>수입 분류</h4>
+        <li v-for="el in IC_data" :key="el.id" v-on:click="IC = el">
+          {{ el.name }}
+        </li>
+      </ul>
+    </div>
+    <br><br>
     <label for="date">date: </label><input type="date" id="date"><br><br>
     <label for="amount">amount: </label><input type="number" id="amount"><br><br>
     <label for="detail">detail: </label><input type="text" id="detail"><br><br>
@@ -20,54 +51,56 @@
 export default {
   data() {
     return {
-      income: true,
-
+      flag: false,
+      aModalFlag: false,
+      cModalFlag: false,
+      IC: { name: '입력하기' },
+      IC_data: [],
+      SC: { name: '입력하기' },
+      SC_data: [],
+      AC: { name: '입력하기' },
+      AC_data: [],
     }
   },
+
   methods: {
     getData() {
       // 소득 분류 가져오기
       this.axios.get(process.env.VUE_APP_API_URL + `/account-book/incomeclassification`)
         .then((res) => {
-          console.log(res.data)
+          this.IC_data = res.data
         })
-        .catch((err) => {console.log(err)})
+        .catch((err) => { console.log(err) })
       // 소비 분류 가져오기
       this.axios.get(process.env.VUE_APP_API_URL + `/account-book/spendingclassification`)
         .then((res) => {
-          console.log(res.data)
+          this.SC_data = res.data
         })
-        .catch((err) => {console.log(err)})
+        .catch((err) => { console.log(err) })
       // 자산 분류 가져오기
-        this.axios.get(process.env.VUE_APP_API_URL + `/account-book/asset`)
+      this.axios.get(process.env.VUE_APP_API_URL + `/account-book/asset`)
         .then((res) => {
-          console.log(res.data)
+          this.AC_data = res.data
         })
-        .catch((err) => {console.log(err)})
-      
+        .catch((err) => { console.log(err) })
+
     },
 
     postData() {
-      if (this.income) {
-        this.spendadd()
-      } else {
+      if (this.flag) {
         this.incomeadd()
+      } else {
+        this.spendadd()
       }
+
     },
     incomeadd() {
-      console.log(document.querySelector("#assetId").value)
-      console.log(document.querySelector("#classificationId").value)
-      console.log(document.querySelector("#date").value)
-      console.log(document.querySelector("#amount").value)
-      console.log(document.querySelector("#detail").value)
-      console.log(document.querySelector("#memo").value)
-
       this.axios({
         method: 'post',
         url: process.env.VUE_APP_API_URL + `/account-book/income`,
         data: {
-          "assetId": document.querySelector("#assetId").value,
-          "classificationId": document.querySelector("#classificationId").value,
+          "assetId": this.AC.id,
+          "classificationId": this.IC.id,
           "date": document.querySelector("#date").value,
           "amount": document.querySelector("#amount").value,
           "detail": document.querySelector("#detail").value,
@@ -82,19 +115,12 @@ export default {
     },
 
     spendadd() {
-      console.log(document.querySelector("#assetId").value)
-      console.log(document.querySelector("#classificationId").value)
-      console.log(document.querySelector("#date").value)
-      console.log(document.querySelector("#amount").value)
-      console.log(document.querySelector("#detail").value)
-      console.log(document.querySelector("#memo").value)
-
       this.axios({
         method: 'post',
         url: process.env.VUE_APP_API_URL + `/account-book/spending`,
         data: {
-          "assetId": document.querySelector("#assetId").value,
-          "classificationId": document.querySelector("#classificationId").value,
+          "assetId": this.AC.id,
+          "classificationId": this.SC.id,
           "date": document.querySelector("#date").value,
           "amount": document.querySelector("#amount").value,
           "detail": document.querySelector("#detail").value,
