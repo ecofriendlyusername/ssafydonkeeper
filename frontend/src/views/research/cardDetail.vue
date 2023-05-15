a2d725fd1156acfa3ec4c3e9b82d45d82aba7490
김 희 현 <gimaeng21@naver.com>
2023-05-15 15:57:02 +0900

    layout:cards

--- frontend/src/views/research/cardDetail.vue
+++ frontend/src/views/research/cardDetail.vue
@@ -1,14 +1,29 @@
 <template>
     <div>
-        <h1>{{ recom_card.name }}</h1>
-        <h3>카드사: {{ recom_card.company }}</h3>
-        <img :src="recom_card.imgPath" :alt="recom_card.name">
-        <h3>전월실적: {{ recom_card.minimumSpending }} / 연회비: {{ recom_card.annualFee }}</h3>
-        <ul>
-            <li v-for="(benefit, idx) in recom_card.benefits?.split('\n')" :key="idx">
+        <div style="width:250px; display: inline-block; margin-top: 20px;">
+            <img :src="recom_card.imgPath" :alt="recom_card.name"
+            style="width:100%">
+        </div>
+        <h2 style="margin-top:10px; margin-bottom: 0px;">{{ recom_card.name }}</h2>
+        <p style="margin:0px;">{{ recom_card.company }}</p>
+        
+        <hr style="border: none; border-top: 3px dotted gray; color:black; background-color: #fff; height: 1px; width: 90%;">
+        <h4 style="margin:0px;">전월실적: {{ recom_card.minimumSpending }} / 연회비: {{ recom_card.annualFee }}</h4>
+        <hr style="border: none; border-top: 3px dotted gray; color:black; background-color: #fff; height: 1px; width: 90%;">
+        
+        <div style="color:orange; font-size:23px;">
+            BENEFIT
+        </div>
+        <div style="font-weight:bold; font-size:17px;">
+            주요혜택
+        </div>
+
+        <div v-for="(benefit, idx) in recom_card.benefits?.split('\n')" :key="idx">
+            <div style="background-color:#F0F2F5; font-weight: bold; letter-spacing:3px; margin: 5px; padding: 5px; border-radius: 5px;">
                 {{ benefit }}
-            </li>
-        </ul>
+            </div>
+        </div>
+
     </div>
 </template>
 
