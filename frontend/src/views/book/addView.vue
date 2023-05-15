75591058977c18c3c2888c696d6b738ce2e24b7f
김 희 현 <gimaeng21@naver.com>
2023-05-15 16:55:00 +0900

    layout:add

--- frontend/src/views/book/addView.vue
+++ frontend/src/views/book/addView.vue
@@ -1,32 +1,52 @@
 <template>
   <div class="DivH">
-    <h2 v-on:click="flag = !flag" v-if="flag">수입</h2>
-    <h2 v-on:click="flag = !flag" v-else>지출</h2>
-    <h4 v-on:click="flag = !flag">바꾸기</h4>
-    <br>
-    <label for="assetId">assetId: </label><div type="text" id="assetId" :value="AC.name"
-      v-on:click="aModalFlag = !aModalFlag">{{ AC.name }}</div>
+    <hr style="height:3px; background-color:black;">
+    <div style="display:flex; margin-top: -20px; justify-content: center; margin-left: 20px; align-items: center;">
+      <div>
+        <h2 v-on:click="flag = !flag" v-if="flag">수입</h2>
+        <h2 v-on:click="flag = !flag" v-else>지출</h2>
+      </div>
+      <div v-on:click="flag = !flag" style="margin-top:5px;">
+        <img src="@/assets/바꾸기.png" style="width:23px; margin: 5px;">
+      </div>
+    </div>
+
+    <hr style="height:1px; background-color:gray; margin-top: -10px;">
+    
+
+    <label for="assetId" style="font-weight:bold; font-size:20px;">
+      소득분류
+    </label>
+    
+      <div type="text" id="assetId" :value="AC.name"
+        v-on:click="aModalFlag = !aModalFlag">
+        {{ AC.name }}
+      </div>
 
     <div class="modal" v-show="aModalFlag">
-      <ul>
-        <h4>소득 분류</h4>
-        <li v-for="el in AC_data" :key="el.id" v-on:click="AC = el">
-          {{ el.name }}
-        </li>
-      </ul>
+      <div>
+        <div v-for="el in AC_data" :key="el.id" v-on:click="AC = el" id="selecadd" style="display:inline-block">
+            <div>{{ el.name }}</div>
+        </div>
+      </div>
     </div>
 
     <br><br>
-    <label for="classificationId">classificationId: </label>
-    <div type="text" id="classificationId" :value="flag ? IC.name : SC.name" v-on:click="cModalFlag = !cModalFlag">{{ flag ? IC.name : SC.name }}</div>
+    <label for="classificationId" style="font-weight:bold; font-size:20px;">
+      지출분류
+    </label>
+
+      <div type="text" id="classificationId" :value="flag ? IC.name : SC.name" v-on:click="cModalFlag = !cModalFlag">
+        {{ flag ? IC.name : SC.name }}
+      </div>
+
 
     <div class="modal" v-show="cModalFlag && !flag">
-      <ul>
-        <h4>지출 분류</h4>
-        <li v-for="el in SC_data" :key="el.id" v-on:click="SC = el">
+      <div>
+        <li v-for="el in SC_data" :key="el.id" v-on:click="SC = el" id="selecadd" style="display:inline-block">
           {{ el.name }}
         </li>
-      </ul>
+      </div>
     </div>
 
     <div class="modal" v-show="cModalFlag && flag">
@@ -38,11 +58,30 @@
       </ul>
     </div>
     <br><br>
-    <label for="date">date: </label><input type="date" id="date"><br><br>
-    <label for="amount">amount: </label><input type="number" id="amount"><br><br>
-    <label for="detail">detail: </label><input type="text" id="detail"><br><br>
-    <label for="memo">memo: </label><input type="text" id="memo"><br><br>
-    <button v-on:click=postData()>저장하기</button>
+
+    <div style="display:flex; padding: 5px;">
+      <div id="addsub"><label for="date">날짜</label></div>
+      <input type="date" id="date" style="border-radius:5px; width: 165px;">
+    </div>
+
+    <div style="display:flex; padding: 5px;">
+      <div id="addsub"><label for="amount">금액</label></div>
+      <input type="number" id="amount" style="border-radius:5px; width: 165px;">
+    </div>
+
+    <div style="display:flex; padding: 5px;">
+      <div id="addsub"><label for="detail">장소</label></div>
+      <input type="text" id="detail" style="border-radius:5px; width: 165px;">
+    </div>
+
+    <div style="display:flex; padding: 5px;">
+      <div id="addsub"><label for="memo">메모</label></div>
+      <input type="text" id="memo" style="border-radius:5px; width: 165px;">
+    </div>
+
+    <br> <br>
+
+    <button v-on:click=postData() id="saveBtn">저장하기</button>
   </div>
 </template>
 
@@ -54,11 +93,11 @@ export default {
       flag: false,
       aModalFlag: false,
       cModalFlag: false,
-      IC: { name: '입력하기' },
+      IC: { name: '선택' },
       IC_data: [],
-      SC: { name: '입력하기' },
+      SC: { name: '선택' },
       SC_data: [],
-      AC: { name: '입력하기' },
+      AC: { name: '선택' },
       AC_data: [],
     }
   },
@@ -145,4 +184,51 @@ export default {
 .DivH {
   min-height: 530px;
 }
+
+#selecadd {
+  width: 100px;
+  background-color:#f7f7f7;
+  padding:5px;
+  margin: 5px;
+  border-radius: 5px;
+}
+
+#assetId {
+  display: inline-block;
+  background-color: #4D82E6;
+  color: white;
+  width: 50px;
+  margin: 5px;
+  font-weight: bold;
+  padding: 5px;
+  border-radius: 10px;
+}
+
+#classificationId {
+  display: inline-block;
+  background-color: #4D82E6;
+  color: white;
+  width: 50px;
+  margin: 5px;
+  font-weight: bold;
+  padding: 5px;
+  border-radius: 10px;
+}
+
+#addsub {
+  width: 180px;
+  font-size: 20px;
+  font-weight: bold;
+}
+
+#saveBtn {
+  background-color: #5E29F6;
+  color: white;
+  padding: 10px;
+  border-radius: 10px;
+  font-weight: bold;
+  border: none;
+}
+
+
 </style>
