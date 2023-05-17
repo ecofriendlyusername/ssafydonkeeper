17ab8631d3735933cc42fd336a451a0d7793f461
gayoung.yang.0 <gayoung.yang.0@gmail.com>
2023-05-17 11:11:20 +0900

    dealing with null case

--- moneykeeperbackend/src/main/java/com/ssafy/moneykeeperbackend/statistics/service/StatService.java
+++ moneykeeperbackend/src/main/java/com/ssafy/moneykeeperbackend/statistics/service/StatService.java
@@ -109,7 +109,9 @@ public class StatService {
             GroupSpending gs = groupSpendingRepository.findByIncomeGroupAndMajorSpendingClassAndYmonth(incomeGroup,msc,ymonth);
 
             if (gs == null) {
-                throw new NoSuchElementException();
+                List<IncomeGroup> igList = incomeGroupRepository.findAll();
+                updateDataService.generateGroupSpending(ymonth,mscList,igList);
+                gs = groupSpendingRepository.findByIncomeGroupAndMajorSpendingClassAndYmonth(incomeGroup,msc,ymonth);
             }
             SpendingDataDto sddGroup = SpendingDataDto.builder()
                     .amount( (int) ((double)gs.getTotal()/(double)gs.getMonths())  )
